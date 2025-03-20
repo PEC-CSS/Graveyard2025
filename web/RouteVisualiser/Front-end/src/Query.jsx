6b56fd39b76b4b8cedb6b9ccae3729f63e159eb7
Anni <157271646+AniketHarit@users.noreply.github.com>
2025-03-20 23:55:58 +0530

    Update Query.jsx

--- web/RouteVisualiser/Front-end/src/Query.jsx
+++ web/RouteVisualiser/Front-end/src/Query.jsx
@@ -25,7 +25,7 @@ function TracerouteMap() {
       }
   
       const data = await response.json();
-      console.log("Traceroute Response:", data); // DEBUGGING
+      console.log("Traceroute Response:", data); 
   
       if (!data.route || data.route.length === 0) {
         alert("No route data found!");
@@ -45,7 +45,7 @@ function TracerouteMap() {
       <input type="text" placeholder="Enter Domain Name" />
       <button onClick={traceRoute}>Traceroute</button>
 
-      {/* Map Component */}
+      {}
       <MapContainer center={[20, 0]} zoom={2} style={{ height: "500px", width: "100%" }}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {route.map((hop, index) => (
